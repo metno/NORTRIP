26e24cb55b1307bb50be75f55f9ee4ae54de1f5c
Elin Aas <elina@met.no>
2024-07-08 12:39:35 +0000

    Do not overwrite the observed surface temperature with value from the init file.

--- NORTRIP_read_init_data_netcdf.f90
+++ NORTRIP_read_init_data_netcdf.f90
@@ -61,6 +61,10 @@ subroutine NORTRIP_read_init_data_netcdf(ncid_init)
     integer, intent(in) :: ncid_init
     integer :: len_track, len_size, len_var, len_roads, len_moist, len_source
 
+    real :: road_meteo_data_tmp(num_road_meteo,n_time,num_track,n_roads)
+    road_meteo_data_tmp = 0
+    !allocate (road_meteo_data(num_road_meteo,n_time,num_track,0:n_roads))
+
     !Set the path and file name
     filename_temp=filename_init_netcdf
     a=date_data(:,min_time)
@@ -77,7 +81,7 @@ subroutine NORTRIP_read_init_data_netcdf(ncid_init)
         if (ro_tot.eq.1) write(unit_logfile,'(A)')' WARNING: Initial netcdf input file does not exist: '//trim(filename)
         return 
     else 
-        write(unit_logfile, '(A)')'Opening netcdf init file for reading'//trim(filename)
+        write(unit_logfile, '(A)')'Opening netcdf init file for reading: '//trim(filename)
     endif
     !If it is the first loop then check if the dimensions match
     if (ro_tot.eq.1) then
@@ -120,9 +124,8 @@ subroutine NORTRIP_read_init_data_netcdf(ncid_init)
     call check(nf90_inq_varid(ncid_init,"M_road_data",varid))
     call check(nf90_get_var(ncid_init,varid, M_road_data(:,:,min_time,tr,0),start = (/1,1,1,ro_tot/)))
 
-    !read road_meteo_data
     call check(nf90_inq_varid(ncid_init,"road_meteo_data",varid))
-    call check(nf90_get_var(ncid_init,varid, road_meteo_data(:,min_time,tr,0),start = (/1,1,ro_tot/)))
+    call check(nf90_get_var(ncid_init,varid, road_meteo_data_tmp(:,1,tr,0),start = (/1,1,ro_tot/)))
 
     !read g_road_data
     call check(nf90_inq_varid(ncid_init,"g_road_data",varid))
@@ -141,6 +144,14 @@ subroutine NORTRIP_read_init_data_netcdf(ncid_init)
 
     call check(nf90_inq_varid(ncid_init,"time_since_last_ploughing",varid))
     call check(nf90_get_var(ncid_init,varid, time_since_last_ploughing(ro),start=(/ro_tot/)))
+
+    !Do not overwrite the observed surface temperature with the init file, bc. it has already been set to observations in 
+    !NORTRIP_multiroad_save_meteodata.f90
+    do i = 1,num_road_meteo
+        if (i .ne. road_temperature_obs_index) then
+            road_meteo_data(i,1,1,0) = road_meteo_data_tmp(i,1,1,0)
+        endif
+    end do
 
     !Check for NaNs in the input data
     do tr=1,num_track
